dd33b580cd951b9b71812beb12aa1dfb3bbc35c7
Eagersheepy <toby.sinclair@errwhat.com>
2018-05-07 08:39:52 -0700

    Added camera :)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/src/main/java/com/app/snapsearch/snapsearch/MainActivity.java
+++ app/src/main/java/com/app/snapsearch/snapsearch/MainActivity.java
@@ -1,13 +1,27 @@
 package com.app.snapsearch.snapsearch;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-//tobytest
+    //tobytest
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.CameraButton);
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(captureImage);
+            }
+        });
+
     }
 }
+
