4ca4bf98e8ded15f5570fe0586d0428c3485710a
varunviswanathan <varunvis@hotmail.com>
2018-05-07 08:55:56 -0700

    minimized image casting

--- app/src/main/java/com/app/snapsearch/snapsearch/MainActivity.java
+++ app/src/main/java/com/app/snapsearch/snapsearch/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Hooks up image button in activity_main to the java object.
-        ImageButton cameraButton = (ImageButton) findViewById(R.id.CameraButton);
+        ImageButton cameraButton = findViewById(R.id.CameraButton);
         //Creates the camera intent.
         final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //Onclick for the camera button moves user to a camera intent to take a photo.
