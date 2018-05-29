800c642fcdcf6da8c4260b9f302b05e6930835a3
Eagersheepy <toby.sinclair@errwhat.com>
2018-05-29 13:37:47 -0700

    all good we think

--- app/src/main/java/com/app/snapsearch/snapsearch/MainActivity.java
+++ app/src/main/java/com/app/snapsearch/snapsearch/MainActivity.java
@@ -1,6 +1,7 @@
 package com.app.snapsearch.snapsearch;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.HttpURLConnection;
 import java.net.URI;
 
@@ -8,12 +9,15 @@ import java.net.URI;
 
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
+import android.graphics.Paint;
+import android.media.Image;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -27,16 +31,19 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import com.microsoft.projectoxford.vision.VisionServiceClient;
 import com.microsoft.projectoxford.vision.VisionServiceRestClient;
 import com.microsoft.projectoxford.vision.contract.AnalysisResult;
 import com.microsoft.projectoxford.vision.contract.Caption;
+import com.microsoft.projectoxford.vision.contract.Tag;
 import com.microsoft.projectoxford.vision.rest.VisionServiceException;
 
 import org.json.JSONObject;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.widget.Toast;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -44,6 +51,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
+import java.security.Key;
 
 import cz.msebera.android.httpclient.client.HttpClient;
 import cz.msebera.android.httpclient.entity.ByteArrayEntity;
@@ -53,78 +61,97 @@ import cz.msebera.android.httpclient.impl.client.HttpClients;
 public class MainActivity extends AppCompatActivity {
     private  static final String TAG =  "ComputerVisionUtil";
     //REMEMBER TO GET KEY OUT OF HERE
-    public  VisionServiceClient visionServiceClient = new VisionServiceRestClient("2ee6b523d54e4bf3af298d87e2314354", "https://westus2.api.cognitive.microsoft.com/vision/v2.0");
-
+    // Parmas for constructor are api key and region of api key.
+    private static final String KEY = "2ee6b523d54e4bf3af298d87e2314354";
+    public  VisionServiceClient visionServiceClient = new VisionServiceRestClient(KEY, "https://westus2.api.cognitive.microsoft.com/vision/v2.0");
+    public static final int CAMERA_REQUEST = 1;
+    Image image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.tree);
-
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        int pictureId = R.drawable.mountain;
+        Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), pictureId);
         // Hooks up image button in activity_main to the java object.
         ImageButton cameraButton = (ImageButton) findViewById(R.id.CameraButton);
-        //Creates the camera intent.
-        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        //Onclick for the camera button moves user to a camera intent to take a photo.
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        mBitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);
         final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        try {
+            cameraButton.setOnClickListener(new View.OnClickListener() {
+                ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+                @Override
+                public void onClick(View v) {
+                    @SuppressLint("StaticFieldLeak") AsyncTask<InputStream, String, String> visonTask = new AsyncTask<InputStream, String, String>() {
+                        @Override
+                        protected String doInBackground(InputStream... fileInputStreams) {
+                            try {
+                                //This describes the loading message
+                                publishProgress("Loading....");
+                                // These lines specify which Json tags will be taken from visionServiceClient.analyzeImage.
+                                String[] features = {"tags"};
+                                String[] details = {};
+                                // The big one, the call to the Image recognition API.
+                                AnalysisResult result = visionServiceClient.analyzeImage(inputStream, features, details);
 
-        // You must use the same region in your REST call as you used to get your
-        // subscription keys. For example, if you got your subscription keys from
-        // westus, replace "westcentralus" in the URI below with "westus".
-        //
-        // Free trial subscription keys are generated in the westcentralus region. If you
-        // use a free trial subscription key, you shouldn't need to change this region.
-        final String uriBase =
-                "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze";
-
-        final String imageToAnalyze =
-                "https://upload.wikimedia.org/wikipedia/commons/" +
-                        "1/12/Broadway_and_Times_Square_by_night.jpg";
-        cameraButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                @SuppressLint("StaticFieldLeak") AsyncTask<InputStream, String, String> visonTask = new AsyncTask<InputStream, String, String>() {
-                    @Override
-                    protected String doInBackground(InputStream... inputStreams) {
-                        try {
-                            publishProgress("Loading....");
-                            String[] features = {"description"};
-                            String[] details = {};
-
-                            AnalysisResult result = visionServiceClient.analyzeImage(imageToAnalyze, features, details);
-
-                            String strResult = new Gson().toJson(result);
-                            return strResult;
-                        } catch (VisionServiceException e) {
-                            e.printStackTrace();
-                            return null;
+                                String strResult = new Gson().toJson(result);
+                                return strResult;
+                                // catching exceptions.
+                            } catch (VisionServiceException e) {
+                                e.printStackTrace();
+                                return null;
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                return null;
+                            }
                         }
-                    }
 
-                    @Override
-                    protected void onPostExecute(String s) {
-                        AnalysisResult result = new Gson().fromJson(s, AnalysisResult.class);
-                        TextView textView = (TextView) findViewById(R.id.textView);
-                        StringBuilder stringBuilder = new StringBuilder();
-                        for (Caption caption : result.description.captions) {
-                            stringBuilder.append(caption.text);
+                        @Override
+                        //Loading message. Both onPreExecute and onProgressUpdate.
+                        protected void onPreExecute() {
+                            mDialog.show();
                         }
-                        textView.setText(stringBuilder);
-                    }
-                };
-                visonTask.execute(inputStream);
-            }
-               // startActivity(captureImage);
+
+                        @Override
+                        protected void onProgressUpdate(String... values) {
+                            mDialog.setMessage(values[0]);
+                        }
+
+                        @Override
+                        //Formats string from tags and sets the textView to the String
+                        protected void onPostExecute(String s) {
+                            mDialog.dismiss();
+                            AnalysisResult result = new Gson().fromJson(s, AnalysisResult.class);
+                            TextView textView = (TextView)findViewById(R.id.textView);
+                            StringBuilder stringBuilder = new StringBuilder();
+
+//                            for(Caption caption : result.description.captions){
+//                                stringBuilder.append(caption.text);
+//                            }
+                            for(Tag tag : result.tags){
+                                stringBuilder.append(tag.name + " ");
+                            }
+
+                            textView.setText(stringBuilder);
+                            Toast.makeText(MainActivity.this, "Fully Loaded", Toast.LENGTH_SHORT).show();
+
+                        }
+                    };
+                    //CALL TO THE ASYNC TASK.
+                    visonTask.execute(inputStream);
+                }
+                // startActivity(captureImage);
                 //puts the date in a bundle stored in a map. value????? use bitmap
                 //captureImage.putExtra(MediaStore.EXTRA_OUTPUT, 0);
-        });
+            });
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
 
     }
-
+}
 //    public void onImageGallerySelected(View v){
 //        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
 //
@@ -143,5 +170,4 @@ public class MainActivity extends AppCompatActivity {
 //
 //    }
 
-}
 
